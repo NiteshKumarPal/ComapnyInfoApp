45a2f4c4557e57b1718969e6a7fa8fd22df8cc95
nitesh-iosdev <nitesh@markmyground.com>
2019-11-29 23:51:43 +0530

    Initial Commit

---
+++ Spectrum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spectrum
+//
+//  Created by Nitesh Kumar Pal on 29/11/19.
+//  Copyright © 2019 Pioneer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
